5fbc0f43ec5b38abe4600ba4ef238248185b577a
Inbal Avidov <inbal.avidov@gmail.com>
2022-12-21 21:11:04 +0200

    marge

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -2,9 +2,9 @@ export const mapService = {
     initMap,
     addMarker,
     panTo,
+    search,
     spliceMarker,
-    addMarkers,
-    search
+    addMarkers
 }
 
 import { locService } from './loc.service.js'
@@ -22,7 +22,7 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
     return _connectGoogleApi()
         .then(() => {
             const myLatlng = { lat, lng }
-            // console.log('google available')
+            console.log('google available')
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
                 center: myLatlng,
@@ -90,6 +90,7 @@ function spliceMarker(id) {
     initMap()
 }
 
+
 function panTo(lat, lng) {
     var laLatLng = new google.maps.LatLng(lat, lng)
     gMap.panTo(laLatLng)
@@ -110,27 +111,6 @@ function _connectGoogleApi() {
     })
 }
 
-// getWeather(31, 31)
-function getWeather(lat, lon) {
-    const KEY = 'weatherDB'
-    const currweather = storageService.load(KEY) || {}
-    if (currweather) return Promise.resolve(currweather)
-
-function search(locName) {
-    const API_KEY_GE0 = `AIzaSyA6WG8zW9hBoHIkuiS2mbS4GQ8zME2jg04`
-    const API_GEO = `https://maps.googleapis.com/maps/api/geocode/json?address=${locName}&key=${API_KEY_GE0}`
-    return axios.get(API_GEO).then(loc => {
-        console.log(locName)
-        console.log(loc.data.results[0].geometry.location)
-    })
-}
-
-
-
-
-
-
-
 
 function search(locName) {
     const API_KEY_GE0 = `AIzaSyA6WG8zW9hBoHIkuiS2mbS4GQ8zME2jg04`
@@ -150,31 +130,4 @@ function search(locName) {
 
 
 
-    console.log('Getting from Network')
-    const API_KEY = '30dc2cce8468139852bf28b9c5bed2a9'
-    return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=metric&lon=${lon}&appid=${API_KEY}`).then(res => {
-        const { name, sys, weather, main, wind } = res.data
-        const { country } = sys
-        const { description } = weather[0]
-        const { temp, temp_min, temp_max } = main
-        const { speed } = wind
-        // console.log(':', sys, weather, main, wind)
-        const requiredWeatherInfo = {
-            country,
-            city: name,
-            // flag,
-            weather: description,
-            AvgTemp: temp,
-            minTemp: temp_min,
-            MaxTemp: temp_max,
-            wind: speed
-        }
-        storageService.save(KEY, requiredWeatherInfo)
-        setTimeout(() => cleanStorage(), 1000 * 60 * 60 * 24)
-        return requiredWeatherInfo
-    })
-}
 
-function cleanStorage() {
-    storageService.save(KEY, null)
-}
